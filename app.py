8be65743d04a56f124593f24f7a6389c1fa0a84a
janumpallydivya15-netizen <janumpallydivya15@gmail.com>
2026-01-21 22:38:56 +0530

    app.py

--- app.py
+++ app.py
@@ -342,6 +342,6 @@ def reports():
 # RUN
 # =================================================
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
